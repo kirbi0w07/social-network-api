fa5296cb6d03c816e9cf8ef45d50916096d3e867
jbarbosac <jose.barbosa@infotec.mx>
2026-04-06 14:09:56 -0500

    auth basics finish

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -29,5 +30,21 @@ class AuthController extends Controller
         ]);
         $token = $user->createToken('auth_token')->plainTextToken;
         return response()->json(['success' => true, 'message' => 'User registered successfully', 'user' => $user, 'token' => $token], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        //desencriptar la contraseña y verificar que coincida con la del usuario
+        $user = User::where('email', $validated['email'])->first();
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            return response()->json(['success' => false, 'message' => 'Invalid credentials'], 401);
+        }
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json(['success' => true, 'message' => 'User logged in successfully', 'user' => $user, 'token' => $token], 200);
     }
 }
