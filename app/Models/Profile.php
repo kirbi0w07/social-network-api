d6c0710c0b22c76e056a9f7563cea907a36d1fa8
Jose Luis Barbosa <kirbi0w0@gmail.com>
2026-08-22 19:58:16 -0500

    fix cover

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\Storage;
+
 #[Fillable(['user_id', 'bio', 'gender', 'birthday', 'username', 'cover_picture'])]
 
 class Profile extends Model
 {
-protected $appends = ['current_avatar', 'cover_picture_url'];
-public function user()
+    protected $appends = ['current_avatar', 'cover_picture_url'];
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
@@ -23,20 +25,16 @@ public function user()
     protected function currentAvatar(): Attribute
     {
         return new Attribute(
-            get: fn () => $this->profilePictures->where('is_current', true)->first(),
+            get: fn() => $this->profilePictures->where('is_current', true)->first(),
         );
     }
 
     protected function coverPictureUrl(): Attribute
-{
-    return new Attribute(
-        get: fn () =>
-            $this->cover_picture
-                ? asset(
-                    'storage/' .
-                    $this->cover_picture
-                  )
+    {
+        return new Attribute(
+            get: fn() => $this->cover_picture
+                ? Storage::url($this->cover_picture)
                 : null,
-    );
-}
+        );
+    }
 }
